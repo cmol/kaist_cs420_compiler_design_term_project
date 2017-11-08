e6dd2d05e2581fc4b8dea6a3518706e6717989a3
Claus Lensbøl <cmol@cmol.dk>
2017-11-08 10:57:48 +0900

    Add all needed types

--- parser_types/parser_types.py
+++ parser_types/parser_types.py
@@ -5,10 +5,76 @@ class Node:
         self.leaf = leaf
 
 class BinOP(Node):
-    def __init__(self,*args):
-        super().__init__(**locals())
+    pass
+#    def __init__(self,*args):
+#        super().__init__(**locals())
 
 class Prog(Node):
-    def __init__(self,*args):
-        super().__init__(**locals())
+    pass
+#    def __init__(self,*args):
+#        super().__init__(**locals())
 
+class Type(Node):
+    pass
+#    def __init__(self,*args):
+#        super().__init__(**locals())
+
+class VarDecl(Node):
+    pass
+#    def __init__(self,*args):
+#        super().__init__(**locals())
+
+class Dcl(Node):
+    pass
+#    def __init__(self,*args):
+#        super().__init__(**locals())
+
+class ParamTypes(Node):
+    pass
+#    def __init__(self,*args):
+#        super().__init__(**locals())
+
+class Func(Node):
+    pass
+#    def __init__(self,*args):
+#        super().__init__(**locals())
+
+class IfStmt(Node):
+    pass
+#    def __init__(self,*args):
+#        super().__init__(**locals())
+
+class WhileStmt(Node):
+    pass
+#    def __init__(self,*args):
+#        super().__init__(**locals())
+
+class ForStmt(Node):
+    pass
+#    def __init__(self,*args):
+#        super().__init__(**locals())
+
+class ReturnStmt(Node):
+    pass
+#    def __init__(self,*args):
+#        super().__init__(**locals())
+
+class Assg(Node):
+    pass
+#    def __init__(self,*args):
+#        super().__init__(**locals())
+
+class CallStmt(Node):
+    pass
+#    def __init__(self,*args):
+#        super().__init__(**locals())
+
+class StmtEnclose(Node):
+    pass
+#    def __init__(self,*args):
+#        super().__init__(**locals())
+
+class Expr(Node):
+    pass
+#    def __init__(self,*args):
+#        super().__init__(**locals())
