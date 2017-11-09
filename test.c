83511c3aae62133269cb2281c955defada5a1484
Claus Lensbøl <cmol@cmol.dk>
2017-11-09 09:39:44 +0900

    Get the file back into spec of C--

--- test.c
+++ test.c
@@ -1,7 +1,11 @@
-int avg(int count, int *value) {
-  int i, total;
-  int sum = 0;
-  for (i = 1; i < count; i++) {
+int c;
+
+int avg(int count, int value) {
+  int i;
+  int total;
+  int sum;
+  sum = 0;
+  for (i = 1; i < count; i = i + 1) {
     total = total + value[i];
   }
   return (total / count);
@@ -10,11 +14,11 @@ int avg(int count, int *value) {
 int main(void) {
   int studentNumber, count, i, sum;
   int mark[4];
-  float average;
+  int average;
   count = 4;
   sum = 0;
   if(0 == 0);
-  for (i = 0; i < count; i++) {
+  for (i = 0; i < count; i = i + 1) {
     mark[i] = i * 30;
     sum = sum + mark[i];
     average = avg(i + 1, mark);
